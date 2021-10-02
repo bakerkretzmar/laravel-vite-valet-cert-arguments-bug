8ab4a7a85d7cdaf21b803725298b75ee32887985
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-10-02 14:43:13 -0400

    Add `.withValetCertificates()`

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,5 @@ import { defineConfig } from "laravel-vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig()
-	.withPlugin(vue);
+    .withPlugin(vue)
+    .withValetCertificates();
